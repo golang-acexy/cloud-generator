7c7471bac19c9c19d4dca6758afe547e3f5f416a
Acexy <thankjava@gmail.com>
2025-06-13 16:22:19 +0800

    generator创建

---
+++ test/simple_demo/gen_test.go
@@ -0,0 +1,41 @@
+package test
+
+import (
+	"github.com/golang-acexy/cloud-generator/generatorcloud"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"testing"
+)
+
+func TestDatabase(t *testing.T) {
+	db, _ := gorm.Open(mysql.Open("root:root@(127.0.0.1:13306)/test?charset=utf8mb4&parseTime=True&loc=Local"))
+	g := generatorcloud.NewGen(db, "/Users/acexy/Repository/github/golang-acexy/cloud-simple-demo/internal/model", []generatorcloud.TableConfig{
+		{
+			TableName: "demo_teacher",
+			ModelName: "Teacher",
+		},
+		{
+			TableName: "demo_student",
+			ModelName: "Student",
+		},
+	})
+	g.SetDTOExcludedFields([]string{
+		"ID",
+		"CreateTime",
+		"UpdateTime",
+	}, []string{
+		"CreateTime",
+		"UpdateTime",
+	}, []string{
+		"ID",
+		"CreateTime",
+		"UpdateTime",
+	})
+	g.SetRepoRelativeModelPath([]string{
+		"..", "service",
+	})
+
+	g.SetModelPkg("github.com/golang-acexy/cloud-simple-demo/internal/model")
+
+	g.Create()
+}
